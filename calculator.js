6057b4f0d28de0a8cca9a29c987bb96d1b30c1b7
Dani-Portillo <159608344+Dani-Portillo@users.noreply.github.com>
2024-04-13 22:49:22 -0600

    Seleccionando elementos del DOM 1 parte
    DOM

---
+++ calculator.js
@@ -0,0 +1,7 @@
+// captura los elementos como un arreglo con sus indices de un arreglo
+const botonNumeros = document.getElementsByName('data-number');
+// console.log(botonNumeros);
+const botonOpera = document.getElementsByName('data-opera');
+const botonIgual = document.getElementsByName('data-igual');
+const botonDelete = document.getElementsByName('data-delete');
+
