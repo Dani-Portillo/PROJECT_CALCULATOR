a13edcf7dcb6e1ad303236969392afbe85bdced2
Dani-Portillo <159608344+Dani-Portillo@users.noreply.github.com>
2024-04-14 22:15:31 -0600

    Capturando elementos y eventos con JS

--- calculator.js
+++ calculator.js
@@ -1,7 +1,34 @@
 // captura los elementos como un arreglo con sus indices de un arreglo
+// const porque no van a cambiar su valor
 const botonNumeros = document.getElementsByName('data-number');
 // console.log(botonNumeros);
 const botonOpera = document.getElementsByName('data-opera');
-const botonIgual = document.getElementsByName('data-igual');
-const botonDelete = document.getElementsByName('data-delete');
+const botonIgual = document.getElementsByName('data-igual')[0]; /**[0] para que no devuelva arreglo */
+const botonDelete = document.getElementsByName('data-delete')[0];
+var result = document.getElementById('result');
+
+botonNumeros.forEach(function (boton){
+    boton.addEventListener('click', function(){
+        // metodo
+        agregarNumero(boton.innerText);
+        // alert(boton.innerText);
+    })
+})
+
+botonOpera.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        selectOperacion(boton.innerText);
+        // alert(boton.innerText);
+    })
+})
+
+botonIgual.addEventListener('click', function(){
+    calcular();
+    actualizarDisplay();
+})
+
+botonDelete.addEventListener('click', function(){
+    clear();
+    actualizarDisplay();
+})
 
